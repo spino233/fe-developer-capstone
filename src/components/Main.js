7d5b1d267fdbdbc2cd4cfbfc8111a023b4d0b99f
Angelo Caci <acaci@tai.it>
2024-05-04 22:42:47 +0200

    created main components, and imported inside App.js

---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+export default function Main(){
+    return (
+        <main>
+            <section>
+                <article>
+                    <h1>Little Lemon</h1>
+                    <h3>Chicago</h3>
+                    <p>
+                        We are a family owned
+                        Mediterranean restaurant,
+                        focused on traditional
+                        recipes served with a modern
+                        twist.
+                    </p>
+                    <button>Reserve a table</button>
+                </article>
+                <div>Food Image</div>
+            </section>
+            <section>
+                Specials
+            </section>
+            <section>
+                Testimonials
+            </section>
+            <section>
+                About
+            </section>
+        </main>
+    )
+}
