259df3999b5a429990aea12108f877970ac6ae2f
Angelo Caci <acaci@tai.it>
2024-05-05 00:27:57 +0200

    finalized html structure for main content and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,34 @@
 export default function Footer(){
     return (
-        <footer></footer>
+        <footer>
+            <section>
+                <img alt='logo'/>
+            </section>
+            <section>
+                <nav>
+                    <h4>Doormat Navigation</h4>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Reservations</li>
+                        <li>Order Online</li>
+                        <li>Login</li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <h4>Contact</h4>
+                <p>Address</p>
+                <p>phone number</p>
+                <p>email</p>
+            </section>
+            <section>
+                <h4>Social Media Links</h4>
+                <a href='https://www.facebook.com/'>Facebook</a>
+                <a href='https://www.instagram.com/'>Instagram</a>
+                <a href='https://www.tiktok.com/en/'>Tik tok</a>
+            </section>
+        </footer>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -14,16 +14,103 @@ export default function Main(){
                     </p>
                     <button>Reserve a table</button>
                 </article>
-                <div>Food Image</div>
+                <img alt='restaurant food'/>
             </section>
             <section>
-                Specials
+                <section>
+                    <h2>Specials</h2>
+                    <button>Online Menu</button>
+                </section>
+                <section>
+                    <article>
+                        <img alt='food'/>
+                        <div className='card-title'>
+                            <h4>Greek Salad</h4>
+                            <span>$12.99</span>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco</p>
+                        <div className='card-action'>
+                            <span>Order for a delivery</span>
+                            <img alt='scooter'/>
+                        </div>
+                    </article>
+                    <article>
+                        <img alt='food'/>
+                        <div className='card-title'>
+                            <h4>Greek Salad</h4>
+                            <span>$12.99</span>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco</p>
+                        <div className='card-action'>
+                            <span>Order for a delivery</span>
+                            <img alt='scooter'/>
+                        </div>
+                    </article>
+                    <article>
+                        <img alt='food'/>
+                        <div className='card-title'>
+                            <h4>Greek Salad</h4>
+                            <span>$12.99</span>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco</p>
+                        <div className='card-action'>
+                            <span>Order for a delivery</span>
+                            <img alt='scooter'/>
+                        </div>
+                    </article>
+                </section>
             </section>
             <section>
-                Testimonials
+                <article>
+                    <h5>Rating</h5>
+                    <div>
+                        <img alt='profile avatar'/>
+                        <h5>Name</h5>
+                    </div>
+                    <p>Leave rating</p>
+                </article>
+                <article>
+                    <h5>Rating</h5>
+                    <div>
+                        <img alt='profile avatar'/>
+                        <h5>Name</h5>
+                    </div>
+                    <p>Leave rating</p>
+                </article>
+                <article>
+                    <h5>Rating</h5>
+                    <div>
+                        <img alt='profile avatar'/>
+                        <h5>Name</h5>
+                    </div>
+                    <p>Leave rating</p>
+                </article>
+                <article>
+                    <h5>Rating</h5>
+                    <div>
+                        <img alt='profile avatar'/>
+                        <h5>Name</h5>
+                    </div>
+                    <p>Leave rating</p>
+                </article>
             </section>
             <section>
-                About
+                <article>
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                </article>
+                <img alt='restaurant 1'/>
+                <img alt='restaurant 2'/>
             </section>
         </main>
     )
